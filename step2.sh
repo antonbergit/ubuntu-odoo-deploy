5b55a91d81cb4a4af8a4b97e640a55fe85473ef1
root <root@Odoo16-ProfiGas.spoc.com.ua>
2024-01-11 16:07:54 +0000

    v16 tested - conf logfile issue

--- step2.sh
+++ step2.sh
@@ -1,10 +1,10 @@
-#git clone https://www.github.com/odoo/odoo --depth 1 --branch 14.0 /opt/odoo/odoo-14.0
-cd /opt/odoo/odoo-14.0
-python3 -m venv /opt/odoo/odoo-14.0/odoo-venv
-source /opt/odoo/odoo-14.0/odoo-venv/bin/activate
+git clone https://www.github.com/odoo/odoo --depth 1 --branch 16.0 /opt/odoo/odoo-16.0
+cd /opt/odoo/odoo-16.0
+python3 -m venv /opt/odoo/odoo-16.0/odoo-venv
+source /opt/odoo/odoo-16.0/odoo-venv/bin/activate
 pip3 install wheel
-pip3 install -r /opt/odoo/odoo-14.0/requirements.txt
+pip3 install -r /opt/odoo/odoo-16.0/requirements.txt
 deactivate
-mkdir /opt/odoo/odoo-14.0/conf
-mkdir /opt/odoo/odoo-14.0/log
+mkdir /opt/odoo/odoo-16.0/conf
+mkdir /opt/odoo/odoo-16.0/log
 
--- step3.sh
+++ step3.sh
@@ -1,35 +1,35 @@
-conf='/opt/odoo/odoo-14.0/conf/odoo14.0.conf'
+conf='/opt/odoo/odoo-16.0/conf/odoo16.0.conf'
 touch $conf
 echo '[options]' > $conf
-echo 'logfile=/opt/odoo/odoo-14.0/log/odoo14.0.log' >> $conf
+echo 'logfile=/opt/odoo/odoo-16.0/log/odoo16.0.log' >> $conf
 echo 'admin_passwd = Spoc2022' >> $conf
 echo 'db_host = False' >> $conf
 echo 'db_port = False' >> $conf
 echo 'db_user = odoo' >> $conf
 echo 'db_password = False' >> $conf
-echo 'addons_path = /opt/odoo/odoo-14.0/odoo/addons,/opt/odoo/odoo-14.0/addons' >> $conf
+echo 'addons_path = /opt/odoo/odoo-16.0/odoo/addons,/opt/odoo/odoo-16.0/addons' >> $conf
 echo 'limit_time_cpu = 6000' >> $conf
 echo 'limit_time_real = 12000' >> $conf
 echo 'max_cron_threads = 1' >> $conf
 echo 'workers = 3' >> $conf
 
-serv='/etc/systemd/system/odoo14.service'
+serv='/etc/systemd/system/odoo16.service'
 touch $serv
 echo '[Unit]' > $serv
-echo 'Description=Odoo 14 CE' >> $serv
+echo 'Description=Odoo 16 CE' >> $serv
 echo 'Requires=postgresql.service' >> $serv
 echo 'After=network.target postgresql.service' >> $serv
 echo '[Service]' >> $serv
 echo 'Type=simple' >> $serv
-echo 'SyslogIdentifier=odoo14' >> $serv
+echo 'SyslogIdentifier=odoo16' >> $serv
 echo 'PermissionsStartOnly=true' >> $serv
 echo 'User=odoo' >> $serv
 echo 'Group=odoo' >> $serv
-echo 'ExecStart=/opt/odoo/odoo-14.0/odoo-venv/bin/python3 /opt/odoo/odoo-14.0/odoo-bin -c /opt/odoo/odoo-14.0/conf/odoo14.0.conf' >> $serv
+echo 'ExecStart=/opt/odoo/odoo-16.0/odoo-venv/bin/python3 /opt/odoo/odoo-16.0/odoo-bin -c /opt/odoo/odoo-16.0/conf/odoo16.0.conf' >> $serv
 echo 'StandardOutput=journal+console' >> $serv
 echo '[Install]' >> $serv
 echo 'WantedBy=multi-user.target' >> $serv
 
 systemctl daemon-reload
-systemctl enable --now odoo14.service
+systemctl enable --now odoo16.service
 
