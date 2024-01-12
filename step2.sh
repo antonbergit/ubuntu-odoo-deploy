b468d32caf5f12097c0b7e374e3cd1619b1d85ec
root <root@Odoo16-ProfiGas.spoc.com.ua>
2024-01-12 03:41:46 +0000

    python 3.8 venv

--- step2.sh
+++ step2.sh
@@ -1,6 +1,6 @@
 git clone https://www.github.com/odoo/odoo --depth 1 --branch 16.0 /opt/odoo/odoo-16.0
 cd /opt/odoo/odoo-16.0
-python3 -m venv /opt/odoo/odoo-16.0/odoo-venv
+python3.8 -m venv /opt/odoo/odoo-16.0/odoo-venv
 source /opt/odoo/odoo-16.0/odoo-venv/bin/activate
 pip3 install wheel
 pip3 install -r /opt/odoo/odoo-16.0/requirements.txt
