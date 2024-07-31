56c1d2fc73713fe85ec725bac8a8d32eaa9b2585
Anton Berh <104967208+antonbergit@users.noreply.github.com>
2024-07-31 15:30:28 +0300

    Update odoo-17.0-dev-install-mini.sh

--- release/odoo-17.0-dev-install-mini.sh
+++ release/odoo-17.0-dev-install-mini.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+addgroup odoo
+usermod -aG odoo $USER
+
 wget -O /tmp/odoo-helper-install.bash https://gitlab.com/katyukha/odoo-helper-scripts/raw/master/install-system.bash;
 bash /tmp/odoo-helper-install.bash;
 odoo-helper install pre-requirements
@@ -9,5 +12,3 @@ odoo-helper install postgres odoo odoo
 odoo-install --install-dir /opt/odoo-17.0 --odoo-version 17.0 --odoo-branch 17.0 --download-archive on --single-branch on --build-python 3.10.13 --http-port 8069 --conf-opt-admin_passwd Bla-blA --conf-opt-workers 3 --conf-opt-proxy_mode False --conf-opt-gevent_port 8072 --sys-deps --ikwid
 
 chown -R $USER:odoo /opt/odoo-17.0
-
-ln -s /opt/odoo-17.0/odoo-helper.conf /home/$USER
